cbb4883afc666b4e2f2f7776e1de8981e8a73d4c
yzyan <yzyan@slave1.pvamucscloud.edu>
2016-05-11 10:19:42 -0400

    [YZ] Run multiple times with 7+3+X

--- tools/runQueryTime.sh
+++ tools/runQueryTime.sh
@@ -4,6 +4,8 @@ SAVEDIR=$PWD
 WORKDIR=$( cd $(dirname $0) ; pwd -P)
 cd $WORKDIR
 
-time java -Dlog.dir=/home/yzyan/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryDB $1
+for (( c=1; c<=$2; c++))
+do time java -Dlog.dir=/home/yzyan/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryDB $1
+done
 
 cd $SAVEDIR
