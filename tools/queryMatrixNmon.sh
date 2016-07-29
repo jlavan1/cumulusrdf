a58766c93a68e92b39651987df06269b3373c86a
jlavan1 <j.lavan@hotmail.com>
2016-07-28 23:01:49 -0500

    Update queryMatrixNmon.sh

--- tools/queryMatrixNmon.sh
+++ tools/queryMatrixNmon.sh
@@ -4,7 +4,7 @@ SAVEDIR=$PWD
 WORKDIR=$( cd $(dirname $0) ; pwd -P)
 cd $WORKDIR
 
-. /home/yzyan/rdf/cumulus/cumulusrdf/tools/nodes.conf
+. /home/jamail/rdf/cumulus/cumulusrdf/tools/nodes.conf
 
 i=0
 len=${#allnodes[@]}
@@ -22,14 +22,14 @@ while [ $i -lt $len ]
 do
     myip=${allnodes[$i]}
 
-    	ssh $myip "rm -rf /home/yzyan/alllogs/$nmondir"
-    	ssh $myip "mkdir /home/yzyan/alllogs/$nmondir"
+    	ssh $myip "rm -rf /home/jamail/alllogs/$nmondir"
+    	ssh $myip "mkdir /home/jamail/alllogs/$nmondir"
 	
-	ssh $myip "nmon -F /home/yzyan/alllogs/$nmondir/log$i.nmon -s1 -c360000"
+	ssh $myip "nmon -F /home/jamail/alllogs/$nmondir/log$i.nmon -s1 -c360000"
    	let i++
 done
 
-java -Dlog.dir=/home/yzyan/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryMatrix $1 $2
+java -Dlog.dir=/home/jamail/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryMatrix $1 $2
 
 
 i=0;
@@ -37,7 +37,7 @@ while [ $i -lt $len ]
 do
 	myip=${allnodes[$i]}
 	ssh $myip "killall -9 nmon"
-	scp -r $myip:/home/yzyan/alllogs/$nmondir/*.nmon ./$nmondir/
+	scp -r $myip:/home/jamail/alllogs/$nmondir/*.nmon ./$nmondir/
 	let i++
 done
 
