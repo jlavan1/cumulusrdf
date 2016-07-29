22e9e68209aabee626eb2ddb82484d42f427dccb
jlavan1 <j.lavan@hotmail.com>
2016-07-28 23:13:13 -0500

    Update updateMatrixTime.sh

--- tools/updateMatrixTime.sh
+++ tools/updateMatrixTime.sh
@@ -4,7 +4,7 @@ SAVEDIR=$PWD
 WORKDIR=$( cd $(dirname $0) ; pwd -P)
 cd $WORKDIR
 
-time java -Dlog.dir=/home/yzyan/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.UpdateMatrix $1 $2
+time java -Dlog.dir=/home/jamail/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.UpdateMatrix $1 $2
 
 
 cd $SAVEDIR
