29524fd4d68827f64abc83527ea2fcadeb4d2726
jlavan1 <j.lavan@hotmail.com>
2016-07-28 22:59:57 -0500

    Update queryAllTime.sh

--- tools/queryAllTime.sh
+++ tools/queryAllTime.sh
@@ -5,7 +5,7 @@ WORKDIR=$( cd $(dirname $0) ; pwd -P)
 cd $WORKDIR
 
 for (( c=1; c<=$2; c++))
-do time java -Dlog.dir=/home/yzyan/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryAll $1
+do time java -Dlog.dir=/home/jamail/rdf/cumulus/cumulusrdf/logs -cp ../cumulusrdf-standalone/target/cumulusrdf-standalone-1.1.0-SNAPSHOT.jar:../cumulusrdf-core/target/cumulusrdf-core-1.1.0-SNAPSHOT.jar:../cumulusrdf-framework/target/cumulusrdf-framework-1.1.0-SNAPSHOT.jar:../cumulusrdf-web-module/target/cumulusrdf-web-module-1.1.0-SNAPSHOT/WEB-INF/lib/* edu.kit.aifb.cumulus.cli.QueryAll $1
 done
 
 cd $SAVEDIR
